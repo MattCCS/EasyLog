c479e835723f251b3541fb8aa647e795ec8ade38
Matt <matt@thecottons.com>
2015-08-01 14:51:34 -0400

    simpler pypi solution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='litelog',
-    version='0.1.4',
+    version='0.1.5',
     author='Matthew Cotton',
     author_email='matt@thecottons.com',
     packages=[
