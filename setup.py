62e92c23e873710f20fe69e8b2b29bddeba64e29
Matt <matt@thecottons.com>
2015-08-01 15:04:48 -0400

    simplified project structure

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from distutils.core import setup
 
 setup(
     name='litelog',
-    version='0.1.5',
+    version='0.1.6',
     author='Matthew Cotton',
     author_email='matt@thecottons.com',
-    packages=[
-        'litelog',
-    ],
+    py_modules=['litelog',],
     # scripts=[],
     url='http://pypi.python.org/pypi/litelog/',
     license='LICENSE.txt',
