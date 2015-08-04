253513d7a068a3bcc84a4e25e22a98009828c5fe
Matt <matt@thecottons.com>
2015-08-04 19:11:04 -0400

    working github/pypi readme!

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup
 
 VERSION_FILE = 'version.txt'
 
-# convert MarkDown to reStructuredText
-try:
-    import pypandoc
-    LONG_DESCRIPTION = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    LONG_DESCRIPTION = open('README.md').read()
-
-# increment dynamic working number
 VERSION = open(VERSION_FILE).read()
+
+def next_version(version):
+    """Increments the version given trivially"""
+    nums = map(int, version.split('.'))
+    (start, last) = nums[:-1], nums[-1]
+    last += 1
+    return '.'.join(map(str, start + [last]))
+
+NEXT_VERSION = next_version(VERSION)
 
 
 setup(
@@ -23,17 +24,11 @@ setup(
     url='http://pypi.python.org/pypi/litelog/',
     license='LICENSE.txt',
     description='Simplified, robust, selective, recursive logging utility for Python.',
-    long_description=LONG_DESCRIPTION,
+    long_description=open('README.rst').read(),
     install_requires=[],
 )
 
 
-def next_version(version):
-    """Increments the version given trivially"""
-    nums = (int(i) for i in version.split('.'))
-    (start, last) = nums[:-1], nums[-1]
-    last += 1
-    return ''.join(start + (last,))
-
+# increment dynamic working number
 with open(VERSION_FILE, 'w') as FILE:
-    FILE.write(next_version(VERSION))
+    FILE.write(NEXT_VERSION)
